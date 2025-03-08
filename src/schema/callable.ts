fc36b3e3482e2b354b4a46802a1db247c3f9591e
Irakli Gozalishvili <contact@gozala.io>
2025-03-07 22:27:26 -0800

    fix: make callable generics possible

--- src/schema/callable.ts
+++ src/schema/callable.ts
@@ -3,9 +3,5 @@ export interface Call<Self extends (...args: any[]) => any> {
 }
 
 export declare const Callable: {
-  new <Self extends (...args: any[]) => any>(
-    self: Self
-  ): {
-    (...args: Parameters<Self>): ReturnType<Self>
-  }
+  new <Self extends (...args: any[]) => any>(self: Self): Self
 }
