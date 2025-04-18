8688390b51d45846e8af187782dab64ef8359052
Irakli Gozalishvili <contact@gozala.io>
2025-04-17 23:14:31 -0700

    fix: generated typeds

--- src/syntax/callable.ts
+++ src/syntax/callable.ts
@@ -1,11 +1,13 @@
-export interface Call<Self extends (...args: any[]) => any> {
-  (...args: Parameters<Self>): ReturnType<Self>
-}
-
-export declare const Callable: {
+// We define this as const because in TS we can't define a callable class
+export declare const ICallable: {
   new <Self extends (...args: any[]) => any>(
     self: Self
   ): {
     (...args: Parameters<Self>): ReturnType<Self>
   }
+}
+
+// We need to declare a class or generated typedefs strip out things that make
+// subclasses appear non-callable.
+export declare class Callable<Self extends (...args: any[]) => any> extends ICallable<Self> {
 }
